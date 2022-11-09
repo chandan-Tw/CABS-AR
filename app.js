6471e8ffb1d12339dbbf9bdc8c6f0c731a78f628
Atshaya Ananthakrishnan <atshaya.a@thoughtworks.com>
2022-11-09 13:33:46 +0530

    [Atshaya]: Added Face tracking with mind-ar & three

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(3000, () =>
+  console.log('Visit http://127.0.0.1:3000')
+);

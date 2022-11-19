0f605cb0d142d27dd6fb3e1f6f2a996eb8c7a5af
Atshaya Ananthakrishnan <atshaya.a@thoughtworks.com>
2022-11-19 19:46:53 +0530

    [Atshaya]: Modified the UI

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ import * as THREE from 'https://unpkg.com/three@0.146.0/build/three.module.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.146.0/examples/jsm/loaders/GLTFLoader.js';
 
 const mindarThree = new window.MINDAR.FACE.MindARThree({
-    container: document.querySelector(".example-container"),
+    container: document.querySelector(".tryon-container"),
 });
 
 const { renderer, scene, camera } = mindarThree;
 
 const light = new THREE.AmbientLight( 0x404040 );
-scene.add( light );
+
 
 renderer.outputEncoding = THREE.sRGBEncoding;
 const start = async () => {
@@ -27,21 +27,22 @@ const remove = () => {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const list = ["hat1"];
+    const list = ["clownHat"];
     const visibles = [false];
     const setVisible = (button, visible) => {
-        const anchor = mindarThree.addAnchor(10);
+        const anchor = mindarThree.addAnchor(168);
         let character;
         const gltfLoader = new GLTFLoader();
         if (visible) {
             button.classList.add("selected");
-            gltfLoader.load("assets/clownHat/scene.gltf", (gltf) => {
-            character = gltf.scene;
-            character.position.set(0, 1.0, -0.5);
+            gltfLoader.load("https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.5/examples/face-tracking/assets/glasses2/scene.gltf", (gltf) => {
+            character = gltf.scene;   //rotation="0 -0 0" position="0 -0.2 -0.5" scale="0.008 0.008 0.008"
+            character.position.set(0, -0.2, -0.5);
             character.rotation.set(0, -0, 0);
-            character.scale.set(0.25, 0.25, 0.25);
+            character.scale.set(0.8, 0.8, 0.8);
             character.visible = visible;
             anchor.group.add(character);
+            scene.add( light );
         });
         } else {
             button.classList.remove("selected");
